ce36c68c4d946f6ba271d2d042340094ef4e60ff
ibrahimliElcin <157967468+ibrahimliElcin@users.noreply.github.com>
2024-02-09 14:37:33 +0400

    grytrt

--- SchoolManagementJDBC/src/main/java/az/perfect/dao/impl/SchoolDaoImpl.java
+++ SchoolManagementJDBC/src/main/java/az/perfect/dao/impl/SchoolDaoImpl.java
@@ -11,8 +11,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
